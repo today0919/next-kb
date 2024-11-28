3fdd5ccbc1262435807694534b246a0de3f1f5eb
today0919 <oha0919@gmail.com>
2024-11-28 17:27:06 +0900

    section2

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <div>
+  <div className="pb-96 flex flex-col gap-4">
     <section>
-      <div>
-        <div className="text-[15vw] font-black"> thinK Great </div>
-
+      <div className="container">
+        <div className="text-[13vw] font-black"> thinK Great </div>
         <div className="relative">
         < Image src="/images/intro1.avif" alt="빌딩사진"
         width={1300} height={1300} className="w-full h-[500px] object-cover" />
@@ -18,6 +18,26 @@ export default function Home() {
             KG그룹광고 <br /> 드디어 TV방송에서 <br /> 만나다.</strong>
           </div>
         </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="container">
+        <ul>
+          <li>
+            <Link href="#" className="flex flex-col gap-3">
+            < Image src="/images/intro2.avif" alt=""width={1300} height={1300}
+            className="w-full aspect-video object-cover"/>
+
+            <div>
+              <span className="text-purple-500 text-md font-medium">소셜미디어</span>
+              <span className="text-gray-300 text-md font-light"> • kg그룹</span>
+              <br />
+              <strong className="text-xl font-bold">소통과 화합의 현장! KG모빌리티 파트너스 데이</strong>
+            </div>
+            </Link>
+          </li>
+        </ul>
       </div>
     </section>
   </div>
